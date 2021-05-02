4b823ded29889dc17e891992902e2bdb4090866c
Josh Jones <ohblonddev@gmail.com>
2021-05-02 14:30:10 +0100

    Tidy up integration test with helpers

--- tests/cli/helpers/mod.rs
+++ tests/cli/helpers/mod.rs
@@ -1,6 +1,7 @@
 use std::path::Path;
 
 use crate::helpers::models::{PourAction, Problem, Solution, SolutionAction};
+use assert_cmd::Command;
 
 pub(crate) mod models;
 
@@ -18,4 +19,24 @@ pub(crate) fn fill(bucket_index: usize) -> SolutionAction {
 
 pub(crate) fn pour(source_bucket_index: usize, target_bucket_index: usize) -> SolutionAction {
     SolutionAction::Pour(PourAction::new(source_bucket_index, target_bucket_index))
+}
+
+pub(crate) fn non_trivial_problem() -> Problem {
+    Problem::new(6, vec![12, 8, 5])
+}
+
+pub(crate) fn valid_solution() -> Solution {
+    Solution::new(vec![
+        fill(0),
+        pour(0, 1),
+        pour(1, 2),
+        pour(2, 0),
+        pour(1, 2),
+        pour(0, 1),
+        pour(1, 2),
+    ])
+}
+
+pub(crate) fn validator_command() -> Command {
+    Command::cargo_bin("validator").unwrap()
 }
--- tests/cli/validates_correct_solution.rs
+++ tests/cli/validates_correct_solution.rs
@@ -1,33 +1,23 @@
 use std::path::PathBuf;
 
-use assert_cmd::Command;
 use assert_fs::prelude::*;
 use assert_fs::TempDir;
 use predicates::str::contains;
 
-use crate::helpers::models::{Problem, Solution};
-use crate::helpers::{fill, pour, write_problem_to_file, write_solution_to_file};
+use crate::helpers::{
+    non_trivial_problem, valid_solution, validator_command, write_problem_to_file,
+    write_solution_to_file,
+};
 
 #[test]
 fn validates_correct_solution() {
     let temp = TempDir::new().unwrap();
     let problem_file_path: PathBuf = temp.child("problem").to_path_buf();
     let solution_file_path: PathBuf = temp.child("solution").to_path_buf();
-    let problem = Problem::new(6, vec![12, 8, 5]);
-    let solution = Solution::new(vec![
-        fill(0),
-        pour(0, 1),
-        pour(1, 2),
-        pour(2, 0),
-        pour(1, 2),
-        pour(0, 1),
-        pour(1, 2),
-    ]);
-    let mut cmd = Command::cargo_bin("validator").unwrap();
-    write_problem_to_file(problem_file_path.as_path(), &problem).unwrap();
-    write_solution_to_file(solution_file_path.as_path(), &solution).unwrap();
+    write_problem_to_file(problem_file_path.as_path(), &non_trivial_problem()).unwrap();
+    write_solution_to_file(solution_file_path.as_path(), &valid_solution()).unwrap();
 
-    let assert = cmd
+    let assert = validator_command()
         .args(&[
             "--problem-file",
             problem_file_path.as_os_str().to_str().unwrap(),
